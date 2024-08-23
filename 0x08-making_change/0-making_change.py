c95900a8dadac811e3133e1e020e64959a567e47
Kipkiruie <elishak551@gmail.com>
2024-08-23 20:09:47 +0300

    making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""Change making module.
+"""Change-making module.
 """
 
 
 def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet a given
     amount total when given a pile of coins of different values.
+
+    Args:
+        coins (list): List of coin values.
+        total (int): The total amount to make change for.
+
+    Returns:
+        int: The fewest number of coins needed to make the change,
+             or -1 if it's not possible to make the change.
     """
     if total <= 0:
-        return 0
-    rem = total
+        return 0  # No coins needed for zero or negative total
+
+    sorted_coins = sorted(coins, reverse=True)  # Sort coins in descending order
     coins_count = 0
-    coin_idx = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
-            return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
+    rem = total
+
+    for coin in sorted_coins:
+        while rem >= coin:
+            rem -= coin
             coins_count += 1
-        else:
-            coin_idx += 1
-    return coins_count
+
+    return coins_count if rem == 0 else -1
